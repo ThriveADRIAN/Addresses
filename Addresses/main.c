6b80c52e10dfd2cf0ba1cb8801cc1a49a792a25f
Adrian Cunanan <adrian.cunanan@me.com>
2013-04-08 14:56:52 -0400

    Exercise Completed

--- Addresses/main.c
+++ Addresses/main.c
@@ -11,8 +11,19 @@
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    printf("Hello, World!\n");
+    int i = 17;
+    int *addressOfI = &i;
+    printf("i stores its value at %p\n", &i);
+    *addressOfI =89;
+    printf("Now i is %d\n", i);
+    printf("An int is %zu bytes\n", sizeof(int));
+    printf("A pointer is %zu bytes\n", sizeof(int *));
+    
+    float *myPointer;
+    // Set myPointer to NULL for now, I'll store a pointer there
+    // later in the program
+    myPointer = NULL;
+    
     return 0;
 }
 
